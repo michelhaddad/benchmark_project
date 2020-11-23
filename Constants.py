4200a111d1e3a3e44f2819fea577797c3a43df19
michelhaddad0 <michel.haddad.intern@anghami.com>
2020-11-23 22:16:46 +0200

    add database manager and constants file

---
+++ Constants.py
@@ -0,0 +1,7 @@
+DB_HOST = 'database-benchmarks.cfxtdgqnjqmp.us-east-2.rds.amazonaws.com'
+DB_USER = 'admin'
+DB_PASSWORD = 'adminadmin'
+DB_NAME = 'db_benchmark'
+PC_TABLE = 'pc'
+BENCHMARK_TABLE = 'benchmarks'
+PC_RESULTS_TABLE = 'pc_benchmark_results'
